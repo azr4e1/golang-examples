b3c46e3e9d71e5dc0dcda23b28dc17dcbe86c3a3
Lorenzo Drumond <lorenzo.drumond@neomediaworld.com>
2024-07-04 11:32:20 +0100

    feat: added base64 decoding

--- CryptoChallenges/firstSet/convertHexToBase64.go
+++ CryptoChallenges/firstSet/convertHexToBase64.go
@@ -6,6 +6,7 @@ import (
 
 const AlphabetLength = 26
 const NumeralLength = 10
+const Base64Max = 64
 
 func encodeHex(b byte) (byte, error) {
 	var out byte
@@ -74,7 +75,7 @@ func FromHex(hx []byte) ([]byte, error) {
 }
 
 func encodeBase64(b byte) (byte, error) {
-	if int(b) >= 64 {
+	if int(b) >= Base64Max {
 		return 0, errors.New("invalid byte")
 	}
 
@@ -85,10 +86,29 @@ func encodeBase64(b byte) (byte, error) {
 		out = byte('a' + ib - AlphabetLength)
 	} else if ib < AlphabetLength+AlphabetLength+NumeralLength {
 		out = byte('0' + ib - AlphabetLength - AlphabetLength)
-	} else if ib == 64-2 { // last two
+	} else if ib == Base64Max-2 { // last two
 		out = '+'
 	} else {
 		out = '/'
+	}
+
+	return out, nil
+}
+
+func decodeBase64(b byte) (byte, error) {
+	var out byte
+	if ib := int(b); ib >= 'A' && ib <= 'Z' {
+		out = byte(ib - 'A')
+	} else if ib >= 'a' && ib <= 'z' {
+		out = byte(ib - 'a' + AlphabetLength)
+	} else if ib >= '0' && ib <= '9' {
+		out = byte(ib - '0' + AlphabetLength + AlphabetLength)
+	} else if b == '+' {
+		out = byte(Base64Max - 2)
+	} else if b == '/' {
+		out = byte(Base64Max - 1)
+	} else {
+		return 0, errors.New("invalid base64 symbol")
 	}
 
 	return out, nil
@@ -151,6 +171,77 @@ func encodeBytesTripletsToBase64(input []byte) ([]byte, error) {
 	return out, nil
 }
 
+func decodeQuartetsToByte(input []byte) ([]byte, error) {
+	out := []byte{}
+	switch len(input) {
+	case 0:
+		return input, nil
+	case 1:
+		// this shouldn't happen in theory
+		first, err := decodeBase64(input[0])
+		if err != nil {
+			return out, err
+		}
+		out = append(out, first)
+	case 2:
+		// result of encoding a single byte
+		b1, b2 := input[0], input[1]
+		first, err := decodeBase64(b1)
+		if err != nil {
+			return out, err
+		}
+		second, err := decodeBase64(b2)
+		if err != nil {
+			return out, err
+		}
+		b := (first << 2) + second
+		out = append(out, b)
+	case 3:
+		// encoding 2 bytes
+		b1, b2, b3 := input[0], input[1], input[2]
+		first, err := decodeBase64(b1)
+		if err != nil {
+			return out, err
+		}
+		second, err := decodeBase64(b2)
+		if err != nil {
+			return out, err
+		}
+		third, err := decodeBase64(b3)
+		if err != nil {
+			return out, err
+		}
+		byte1 := (first << 2) + (second >> 4)
+		byte2 := (second << 4) + third
+		out = append(out, byte1, byte2)
+	case 4:
+		// encoding 3 bytes
+		b1, b2, b3, b4 := input[0], input[1], input[2], input[3]
+		first, err := decodeBase64(b1)
+		if err != nil {
+			return out, err
+		}
+		second, err := decodeBase64(b2)
+		if err != nil {
+			return out, err
+		}
+		third, err := decodeBase64(b3)
+		if err != nil {
+			return out, err
+		}
+		fourth, err := decodeBase64(b4)
+		if err != nil {
+			return out, err
+		}
+		byte1 := (first << 2) + (second >> 4)
+		byte2 := (second << 4) + (third >> 2)
+		byte3 := (third << 6) + fourth
+		out = append(out, byte1, byte2, byte3)
+	}
+
+	return out, nil
+}
+
 func ToBase64(input []byte) ([]byte, error) {
 	var toEncode []byte
 	out := []byte{}
@@ -163,6 +254,26 @@ func ToBase64(input []byte) ([]byte, error) {
 		out = append(out, encodedBytes...)
 	}
 	encodedBytes, err := encodeBytesTripletsToBase64(input)
+	if err != nil {
+		return out, err
+	}
+	out = append(out, encodedBytes...)
+
+	return out, nil
+}
+
+func FromBase64(input []byte) ([]byte, error) {
+	var toEncode []byte
+	out := []byte{}
+	for len(input) > 3 {
+		toEncode, input = input[:4], input[4:]
+		encodedBytes, err := decodeQuartetsToByte(toEncode)
+		if err != nil {
+			return out, err
+		}
+		out = append(out, encodedBytes...)
+	}
+	encodedBytes, err := decodeQuartetsToByte(input)
 	if err != nil {
 		return out, err
 	}
